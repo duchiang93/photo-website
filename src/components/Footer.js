c685657cba8356bc5e1c70cf2ae209e3460a2c1c
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-06-06 15:35:40 +0800

    0606

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Footer = () => {
+  return <div className="footer">Sharon 2022©</div>;
+};
+
+export default Footer;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../components/Search";
+
+const Homepage = () => {
+  const auth = "563492ad6f917000010000010facb0d82cca434786cabbc103150ed0";
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
